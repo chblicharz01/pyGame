b2b9141e65e6dbfe542aaa3ca065d5226cfb94c7
Chris Blicharz <chblicharz01@gmail.com>
2022-07-20 14:56:24 -0500

    brick game

---
+++ Scripts/chapter_11/brick.py
@@ -0,0 +1,58 @@
+#authored by Sloan Kelly
+#modified by Chris Blicharz
+
+import pygame, os, sys
+from pygame.locals import *
+
+pygame.init()
+fpsClock = pygame.time.Clock()
+mainSurface = pygame.display.set_mode((800,600))
+pygame.display.set_caption('Bricks')
+
+black = pygame.Color(0,0,0)
+
+#controllable bat
+
+bat = pygame.image.load('bat.png')
+playerY = 540 #y coordinate is constant at 540
+
+batRect = bat.get_rect()
+mousex,mousey = (0, playerY)
+
+#ball init
+ball = pygame.image.load('ball.png')
+ballRect = ball.get_rect()
+ballStartY = 200
+ballSpeed = 3
+ballServed = False
+bx, by = (24,ballStartY)
+sx, sy = (ballSpeed, ballSpeed)
+ballRect.topleft = (bx,by)
+
+#brick init
+
+while True:
+    mainSurface.fill(black)
+
+    #draw bat
+    mainSurface.blit(bat, batRect)
+
+    #brick draw
+    mainSurface.blit(ball,ballRect)
+
+    #events
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == MOUSEMOTION:
+            mousex,mousey = event.pos
+            if(mousex < 800-55):
+                batRect.topleft = (mousex, playerY)
+            else:
+                batRect.topleft = (800 - 55, playerY)
+        #main game logic
+        #collision detection
+        pygame.display.update()
+        fpsClock.tick(30)
+
