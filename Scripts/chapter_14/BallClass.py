2f878cfa395eef42a6a99c06de1cfd5fc61d6046
Chris Blicharz <chblicharz01@gmail.com>
2022-07-28 14:04:47 -0500

    brick game

--- Scripts/chapter_14/BallClass.py
+++ Scripts/chapter_14/BallClass.py
@@ -15,3 +15,20 @@ class Ball:
         return False
     def draw(self, gameTime, surface):
         surface.blit(self.img, (self.x, self.y))
+
+if __name__ == '__main__':
+    pygame.init()
+    fpsClock = pygame.time.Clock()
+    surface = pygame.display.set_mode((800,600))
+
+    ball = Ball()
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+            ball.update(fpsClock)
+            surface.fill((0,0,0))
+            ball.draw(fpsClock, surface)
+
